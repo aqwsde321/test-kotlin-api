291e544789cf2a4beca5eb0c95714bca3bef18a2
qrqrq23r <qrqrq23r@gmail.com>
2025-06-29 22:43:10 +0900

    ExceptionHandler 만들기

--- src/main/kotlin/com/example/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/example/study/blog/service/BlogService.kt
@@ -1,6 +1,7 @@
 package com.example.study.blog.service
 
 import com.example.study.blog.dto.BlogDto
+import com.example.study.core.exception.InvalidInputException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
@@ -8,11 +9,31 @@ import org.springframework.web.reactive.function.client.bodyToMono
 
 
 @Service
-class BlogService (
+class BlogService(
     @Qualifier("kakaoWebClient")
     private val webClient: WebClient
-){
+) {
     fun searchKakao(blogDto: BlogDto): String? {
+        val msgList = mutableListOf<ExceptionMsg>()
+
+        if (blogDto.query.trim().isEmpty()) {
+            msgList.add(ExceptionMsg.EMPTY_QUERY)
+        }
+
+        if (blogDto.sort.trim() !in arrayOf("accuracy", "recency")) {
+            msgList.add(ExceptionMsg.NOT_IN_SORT)
+        }
+
+        when {
+            blogDto.page < 1 -> msgList.add(ExceptionMsg.LESS_THAN_MIN)
+            blogDto.page > 50 -> msgList.add(ExceptionMsg.MORE_THEN_MAX)
+        }
+
+        if (msgList.isNotEmpty()) {
+            val message = msgList.joinToString {it.msg}
+            throw InvalidInputException(message)
+        }
+
         val response = webClient.get()
             .uri {
                 it.path("/v2/search/blog")
@@ -30,4 +51,12 @@ class BlogService (
         return result
 
     }
+}
+
+private enum class ExceptionMsg(val msg: String) {
+    EMPTY_QUERY("Empty Query"),
+    NOT_IN_SORT("NotInSort By Sort"),
+    LESS_THAN_MIN("page is less than min"),
+    MORE_THEN_MAX("Page is more than max"),
+
 }
